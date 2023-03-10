2b83d2d17cee5cecdc91204a583049bf3702eb60
LONEWOLF99 <90772125+Utkarsh-1104@users.noreply.github.com>
2023-03-10 21:11:53 +0530

    Add files via upload

---
+++ AVG_OF_ARRAY_ELE.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,avg=0;
+    cout<<("Enter the number of the elements of the array : ");
+    cin>>i;
+    int A[i];
+    for(auto& x:A)
+        cin>>x;
+    for(auto x:A)
+        avg+=x;
+    avg/=i;
+    cout<<("The average of the elements of the array is : ")<<avg;
+    return 0;
+}
