080f931058421fba1cbce61f144fc42fa6465096
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2023-03-14 09:07:17 +0530

    .

---
+++ FUNC_OVERLL.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+float sum(int x, float y)
+{
+    cout<<x<<" "<<y;
+    return x+y;
+}
+float sum(float m, int n)
+{
+    cout<<m<<" "<<n;
+    return m+n;
+}
+int main()
+{
+    int a; float b;
+    cout<<("Enter two numbers : ");
+    cin>>a>>b;
+    float (c) = sum(a,b);
+    cout<<c<<endl;
+    float d = sum(b,a);
+    cout<<d;
+}
