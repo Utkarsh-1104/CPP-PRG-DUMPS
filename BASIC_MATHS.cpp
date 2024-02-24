c4d1159ec33c2ac0ca6ee6635306b0ce6439459c
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-02-24 20:54:22 +0530

    GCD or HCF

--- BASIC_MATHS.cpp
+++ BASIC_MATHS.cpp
@@ -18,6 +18,29 @@ int countDigits() {
     }
 }
 
+//GCD or HCF
+//brute force
+// int GCD() {
+//     int m,n;
+//     cin>>m>>n;
+//     int min;
+//     int gcd=1;
+//     (m>n) ? min = n : min = m;
+//     for (int i = 1; i <= min; i++)
+//     {
+//         if (m % i == 0 && n % i == 0) gcd = i;
+//     }
+//     return gcd;
+// }
+
+//euclid's algo
+int GCD(int n, int m) {
+    if (m==0)
+        return n;
+    return GCD(m, n % m);
+}
+
 int main() {
-    cout<<countDigits();
+    cout<<countDigits()<<endl;
+    cout<<GCD(3, 2);
 }
