e27f1d7bce96a11bf40be2513b933118966f4319
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-02-25 08:41:21 +0530

    reverse

--- BASIC_MATHS.cpp
+++ BASIC_MATHS.cpp
@@ -71,10 +71,21 @@ bool isPrime(int n)
 	else return false;
 }
 
+//reverse number
+int reverse(int x) {
+    long num = 0;
+    while(x != 0) {
+        num = num*10 + x%10;
+        x/=10;
+    }
+    if(num>INT_MAX || num<INT_MIN) return 0;
+    return int(num);
+}
 
 int main() {
     cout<<countDigits()<<endl;
     cout<<GCD(3, 2);
     cout<<checkArmstrong();
     cout<<isPrime(8);
+    cout<<reverse(123);
 }
