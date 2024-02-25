f31002d5465e69ab1951790cbe91ad1a8f346a2e
LONEWOLF99 <bajpaiutkarsh1104@gmail.com>
2024-02-25 08:37:59 +0530

    prime number

--- BASIC_MATHS.cpp
+++ BASIC_MATHS.cpp
@@ -57,8 +57,24 @@ bool checkArmstrong(){
 		return false;
 }
 
+//prime
+bool isPrime(int n)
+{
+	int count = 0;
+	for(int i = 1; i*i <= n; i++) {
+		if(n % i == 0) {
+			count++;
+			if((n/i) != i) count++;
+		}
+	}
+	if(count == 2) return true;
+	else return false;
+}
+
+
 int main() {
-    // cout<<countDigits()<<endl;
-    // cout<<GCD(3, 2);
+    cout<<countDigits()<<endl;
+    cout<<GCD(3, 2);
     cout<<checkArmstrong();
+    cout<<isPrime(8);
 }
